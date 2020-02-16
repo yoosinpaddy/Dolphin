862770d7a4baab28b9aa10500e44af91abadece1
Noblepal <zeronillzero@gmail.com>
2020-02-16 23:03:43 +0300

    Basic Design

--- app/src/main/java/com/trichain/dolphin/activities/MainActivity.java
+++ app/src/main/java/com/trichain/dolphin/activities/MainActivity.java
@@ -1,9 +1,12 @@
 package com.trichain.dolphin.activities;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -17,14 +20,18 @@ import com.trichain.dolphin.model.Player;
 import java.util.ArrayList;
 import java.util.Arrays;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     ArrayList<Player> players = new ArrayList<>();
+    Button btnAddPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnAddPlayer = findViewById(R.id.btnAddplayer);
+        btnAddPlayer.setOnClickListener(this);
 
         players.addAll(Arrays.asList(
                 new Player("John Doe", 1, 0),
@@ -54,6 +61,33 @@ public class MainActivity extends AppCompatActivity {
                 return players.size();
             }
         });
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == btnAddPlayer) {
+            showAddPlayerDialog();
+        }
+    }
+
+    private void showAddPlayerDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        View dialogView = LayoutInflater.from(this).inflate(R.layout.rec_player_entry, null);
+        builder.setView(dialogView);
+        builder.setPositiveButton("Add", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                //TODO: Add new player and update recyclerView adapter
+            }
+        });
+        builder.setNegativeButton("Dismiss", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+        builder.show();
+
     }
 
     public class MyViewHolder extends RecyclerView.ViewHolder {
